71d56a046393862f6a4cc713d2996b1d8cdfe607
Alex Burneikis <z5481067@ad.unsw.edu.au>
2025-08-08 16:13:17 +1000

    step 1

---
+++ src/main/java/com/moreoutlines/mixins/EntityMixin.java
@@ -0,0 +1,20 @@
+package com.moreoutlines.mixins;
+
+import com.moreoutlines.config.ModConfig;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    
+    @Inject(method = "isGlowing", at = @At("HEAD"), cancellable = true)
+    private void onIsGlowing(CallbackInfoReturnable<Boolean> cir) {
+        if (ModConfig.INSTANCE.itemOutlines && (Entity) (Object) this instanceof ItemEntity) {
+            cir.setReturnValue(true);
+        }
+    }
+}
